2541ed8207278cb8f8d87100f36f5cf57ffd41d0
ShockRate <blackhound05@yahoo.gr>
2016-12-21 01:06:26 +0200

    Commiing the project

---
+++ client/controllers/books.js
@@ -0,0 +1,50 @@
+var myApp = angular.module('myApp');
+
+myApp.controller('BooksController',['$scope', '$http', '$location', '$routeParams',
+function($scope, $http, $location, $routeParams){
+	console.log('BooksController loaded ...');
+	$scope.getBooks = function(){
+		$http.get('/api/books').success(function(response){
+			$scope.books = response;
+		});
+	}
+
+$scope.getBook = function(){
+	var id = $routeParams.id;
+		$http.get('/api/books/'+id).success(function(response){
+			$scope.book = response;
+
+		});
+	}
+
+	$scope.addBook = function(){
+		console.log($scope.book);
+		$http.post('/api/books/', $scope.book).success(function(response){
+			
+			window.location.href='#/books';
+		});
+	}
+
+	$scope.updateBook = function(){
+		console.log($scope.book);
+		var id = $routeParams.id;
+		$http.put('/api/books/'+id, $scope.book).success(function(response){
+			
+			window.location.href='#/books';
+		});
+	}
+
+	$scope.removeBook = function(id){
+		var confirmation = confirm('Are you sure?');
+
+		if (confirmation) {
+		
+
+	
+		$http.delete('/api/books/'+id).success(function(response){
+			window.location.href='#/books';
+		});
+	}
+
+	}
+}]);
